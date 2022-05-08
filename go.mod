e8662c414ad8e4884571380ce1b60f0e3885c938
bluffy <mario@bluffy.de>
2022-05-08 19:26:12 +0200

    jitsi openid

--- go.mod
+++ go.mod
@@ -4,12 +4,21 @@ go 1.17
 
 require (
 	github.com/coreos/go-oidc/v3 v3.1.0
+	github.com/joho/godotenv v1.4.0
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 )
 
 require (
+	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20211113050330-71f90109db02 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli v1.22.9 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- main.go
+++ main.go
@@ -8,23 +8,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
 
 var (
-	JITSI_SECRET = os.Getenv("JITSI_SECRET")
-	JITSI_URL    = os.Getenv("JITSI_URL")
-	JITSI_SUB    = os.Getenv("JITSI_SUB")
+	JITSI_SECRET = goDotEnvVariable("JITSI_SECRET")
+	JITSI_URL    = goDotEnvVariable("JITSI_URL")
+	JITSI_SUB    = goDotEnvVariable("JITSI_SUB")
 
-	ISSUER_BASE_URL = os.Getenv("ISSUER_BASE_URL")
-	BASE_URL        = os.Getenv("BASE_URL")
-	CLIENT_ID       = os.Getenv("CLIENT_ID")
-	SECRET          = os.Getenv("SECRET")
+	ISSUER_BASE_URL = goDotEnvVariable("ISSUER_BASE_URL")
+	BASE_URL        = goDotEnvVariable("BASE_URL")
+	CLIENT_ID       = goDotEnvVariable("CLIENT_ID")
+	SECRET          = goDotEnvVariable("SECRET")
 )
+
+func init() {
+	// load .env file
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load(".env")
+
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+	}
+}
+
+func goDotEnvVariable(key string) string {
+
+	return os.Getenv(key)
+}
 
 func randString(nByte int) (string, error) {
 	b := make([]byte, nByte)
@@ -45,99 +63,119 @@ func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value strin
 	http.SetCookie(w, c)
 }
 
-func main() {
-	ctx := context.Background()
+var ctx context.Context
+var provider *oidc.Provider
+var config oauth2.Config
 
-	provider, err := oidc.NewProvider(ctx, ISSUER_BASE_URL)
+func handleStart(w http.ResponseWriter, r *http.Request) {
+
+	state, err := randString(16)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
 	}
+	nonce, err := randString(16)
+	if err != nil {
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+	setCallbackCookie(w, r, "state", state)
+	setCallbackCookie(w, r, "nonce", nonce)
+
+	http.Redirect(w, r, config.AuthCodeURL(state, oidc.Nonce(nonce)), http.StatusFound)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("TEST " + CLIENT_ID)
+
+	state, err := r.Cookie("state")
+	if err != nil {
+		http.Error(w, "state not found", http.StatusBadRequest)
+		return
+	}
+	if r.URL.Query().Get("state") != state.Value {
+		http.Error(w, "state did not match", http.StatusBadRequest)
+		return
+	}
+
+	//w.Write([]byte(r.URL.RawQuery))
+
+	oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+		return
+	}
+
 	oidcConfig := &oidc.Config{
 		ClientID: CLIENT_ID,
 	}
 	verifier := provider.Verifier(oidcConfig)
 
-	config := oauth2.Config{
+	idToken, err := verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	nonce, err := r.Cookie("nonce")
+	if err != nil {
+		http.Error(w, "nonce not found", http.StatusBadRequest)
+		return
+	}
+	if idToken.Nonce != nonce.Value {
+		http.Error(w, "nonce did not match", http.StatusBadRequest)
+		return
+	}
+
+	oauth2Token.AccessToken = "*REDACTED*"
+
+	resp := struct {
+		OAuth2Token   *oauth2.Token
+		IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+	}{oauth2Token, new(json.RawMessage)}
+
+	if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data, err := json.MarshalIndent(resp, "", "    ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+
+}
+
+func main() {
+	ctx = context.Background()
+	var err error
+	provider, err = oidc.NewProvider(ctx, ISSUER_BASE_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redir := strings.Trim(BASE_URL, "/") + "/callback"
+
+	config = oauth2.Config{
 		ClientID:     CLIENT_ID,
 		ClientSecret: SECRET,
 		Endpoint:     provider.Endpoint(),
-		RedirectURL:  BASE_URL + "/callback",
+		RedirectURL:  redir,
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		state, err := randString(16)
-		if err != nil {
-			http.Error(w, "Internal error", http.StatusInternalServerError)
-			return
-		}
-		nonce, err := randString(16)
-		if err != nil {
-			http.Error(w, "Internal error", http.StatusInternalServerError)
-			return
-		}
-		setCallbackCookie(w, r, "state", state)
-		setCallbackCookie(w, r, "nonce", nonce)
+	http.HandleFunc("/", handleStart)
 
-		http.Redirect(w, r, config.AuthCodeURL(state, oidc.Nonce(nonce)), http.StatusFound)
-	})
+	http.HandleFunc("/callback", handleCallback)
 
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		state, err := r.Cookie("state")
-		if err != nil {
-			http.Error(w, "state not found", http.StatusBadRequest)
-			return
-		}
-		if r.URL.Query().Get("state") != state.Value {
-			http.Error(w, "state did not match", http.StatusBadRequest)
-			return
-		}
-
-		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
-		if err != nil {
-			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
-		if !ok {
-			http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
-			return
-		}
-		idToken, err := verifier.Verify(ctx, rawIDToken)
-		if err != nil {
-			http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		nonce, err := r.Cookie("nonce")
-		if err != nil {
-			http.Error(w, "nonce not found", http.StatusBadRequest)
-			return
-		}
-		if idToken.Nonce != nonce.Value {
-			http.Error(w, "nonce did not match", http.StatusBadRequest)
-			return
-		}
-
-		oauth2Token.AccessToken = "*REDACTED*"
-
-		resp := struct {
-			OAuth2Token   *oauth2.Token
-			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
-		}{oauth2Token, new(json.RawMessage)}
-
-		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		data, err := json.MarshalIndent(resp, "", "    ")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
-	})
-
-	log.Printf("listening on http://%s/", "127.0.0.1:3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Printf("listening on http://%s/", "0.0.0.0:3001")
+	log.Fatal(http.ListenAndServe(":3001", nil))
 }
