3fca104b6ed171d122a296d97dfa8ae295bf7654
bluffy <mario@bluffy.de>
2022-05-08 21:18:38 +0200

    op

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/joho/godotenv"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
@@ -28,8 +30,13 @@ var (
 	SECRET          = goDotEnvVariable("SECRET")
 )
 
+type PlayLoad struct {
+	ID    string `json:"sub"`
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
 func init() {
-	// load .env file
 	if _, err := os.Stat(".env"); err == nil {
 		err := godotenv.Load(".env")
 
@@ -52,130 +59,188 @@ func randString(nByte int) (string, error) {
 	return base64.RawURLEncoding.EncodeToString(b), nil
 }
 
-func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value string) {
-	c := &http.Cookie{
-		Name:     name,
-		Value:    value,
-		MaxAge:   int(time.Hour.Seconds()),
-		Secure:   r.TLS != nil,
-		HttpOnly: true,
-	}
-	http.SetCookie(w, c)
-}
-
-var ctx context.Context
-var provider *oidc.Provider
-var config oauth2.Config
-
-func handleStart(w http.ResponseWriter, r *http.Request) {
-
-	state, err := randString(16)
-	if err != nil {
-		http.Error(w, "Internal error", http.StatusInternalServerError)
-		return
-	}
-	nonce, err := randString(16)
-	if err != nil {
-		http.Error(w, "Internal error", http.StatusInternalServerError)
-		return
-	}
-	setCallbackCookie(w, r, "state", state)
-	setCallbackCookie(w, r, "nonce", nonce)
-
-	http.Redirect(w, r, config.AuthCodeURL(state, oidc.Nonce(nonce)), http.StatusFound)
-}
-
-func handleCallback(w http.ResponseWriter, r *http.Request) {
-
-	log.Println("TEST " + CLIENT_ID)
-
-	state, err := r.Cookie("state")
-	if err != nil {
-		http.Error(w, "state not found", http.StatusBadRequest)
-		return
-	}
-	if r.URL.Query().Get("state") != state.Value {
-		http.Error(w, "state did not match", http.StatusBadRequest)
-		return
-	}
-
-	//w.Write([]byte(r.URL.RawQuery))
-
-	oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
-	if err != nil {
-		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
-	if !ok {
-		http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
-		return
-	}
-
-	oidcConfig := &oidc.Config{
-		ClientID: CLIENT_ID,
-	}
-	verifier := provider.Verifier(oidcConfig)
-
-	idToken, err := verifier.Verify(ctx, rawIDToken)
-	if err != nil {
-		http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	nonce, err := r.Cookie("nonce")
-	if err != nil {
-		http.Error(w, "nonce not found", http.StatusBadRequest)
-		return
-	}
-	if idToken.Nonce != nonce.Value {
-		http.Error(w, "nonce did not match", http.StatusBadRequest)
-		return
-	}
-
-	oauth2Token.AccessToken = "*REDACTED*"
-
-	resp := struct {
-		OAuth2Token   *oauth2.Token
-		IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
-	}{oauth2Token, new(json.RawMessage)}
-
-	if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	data, err := json.MarshalIndent(resp, "", "    ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(data)
-
-}
-
 func main() {
-	ctx = context.Background()
-	var err error
-	provider, err = oidc.NewProvider(ctx, ISSUER_BASE_URL)
+
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, ISSUER_BASE_URL)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	redir := strings.Trim(BASE_URL, "/") + "/callback"
-
-	config = oauth2.Config{
+	config := oauth2.Config{
 		ClientID:     CLIENT_ID,
 		ClientSecret: SECRET,
 		Endpoint:     provider.Endpoint(),
-		RedirectURL:  redir,
+		RedirectURL:  strings.Trim(BASE_URL, "/") + "/callback",
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
 	}
 
-	http.HandleFunc("/", handleStart)
+	r := gin.Default()
 
-	http.HandleFunc("/callback", handleCallback)
+	r.GET("/room/:room", func(c *gin.Context) {
 
-	log.Printf("listening on http://%s/", "0.0.0.0:3001")
-	log.Fatal(http.ListenAndServe(":3001", nil))
+		room := c.Param("room")
+
+		state, err := randString(16)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		nonce, err := randString(16)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		c.SetCookie("state", state, int(time.Hour.Seconds()), "/", "", c.Request.TLS != nil, true)
+		c.SetCookie("nonce", nonce, int(time.Hour.Seconds()), "/", "", c.Request.TLS != nil, true)
+		c.SetCookie("room", room, int(time.Hour.Seconds()), "/", "", c.Request.TLS != nil, true)
+		c.Redirect(http.StatusFound, config.AuthCodeURL(state, oidc.Nonce(nonce)))
+	})
+
+	r.GET("/callback", func(c *gin.Context) {
+
+		log.Println("TEST " + CLIENT_ID)
+
+		state, err := c.Cookie("state")
+		if err != nil {
+			c.String(http.StatusInternalServerError, "state not found")
+			return
+		}
+		if c.Query("state") != state {
+			c.String(http.StatusInternalServerError, "state did not match")
+			return
+		}
+		c.SetCookie("state", "", -1, "/", "", c.Request.TLS != nil, true)
+
+		room, err := c.Cookie("room")
+		if err != nil {
+			c.String(http.StatusInternalServerError, "state not set")
+			return
+		}
+		c.SetCookie("room", "", -1, "/", "", c.Request.TLS != nil, true)
+
+		//w.Write([]byte(r.URL.RawQuery))
+
+		oauth2Token, err := config.Exchange(ctx, c.Query("code"))
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to exchange token: "+err.Error())
+			return
+		}
+
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			c.String(http.StatusInternalServerError, "No id_token field in oauth2 token.")
+			return
+		}
+
+		oidcConfig := &oidc.Config{
+			ClientID: CLIENT_ID,
+		}
+		verifier := provider.Verifier(oidcConfig)
+
+		idToken, err := verifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to verify ID Token: "+err.Error())
+
+			return
+		}
+
+		nonce, err := c.Cookie("nonce")
+		if err != nil {
+			c.String(http.StatusInternalServerError, "nonce not found")
+
+			return
+		}
+		if idToken.Nonce != nonce {
+			c.String(http.StatusBadRequest, "nonce did not match")
+			return
+		}
+		c.SetCookie("nonce", "", -1, "/", "", c.Request.TLS != nil, true)
+
+		oauth2Token.AccessToken = "*REDACTED*"
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+		}{oauth2Token, new(json.RawMessage)}
+
+		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		/*
+			data, err := json.MarshalIndent(resp, "", "    ")
+			if err != nil {
+				c.String(http.StatusInternalServerError, err.Error())
+				return
+			}
+		*/
+
+		var playLoad PlayLoad
+		err = json.Unmarshal(*resp.IDTokenClaims, &playLoad)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		claims := jwt.MapClaims{}
+		claims["exp"] = time.Now().Add(time.Hour * 24 * 30).Unix()
+		claims["aud"] = "jitsi"
+		claims["sub"] = JITSI_SUB
+		claims["iss"] = "jitsi"
+		claims["room"] = room
+		claims["context"] = `{
+			"user": {
+				"name": ` + playLoad.Name + `,
+				"email": ` + playLoad.Email + `,
+				"id": ` + playLoad.ID + `
+			}
+		}`
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		tokenString, err := token.SignedString([]byte(JITSI_SECRET))
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		c.Redirect(http.StatusFound, JITSI_URL+"/room/"+room+"?jwt="+tokenString)
+
+		/*
+
+			data, err := json.MarshalIndent(resp, "", "    ")
+			if err != nil {
+				c.String(http.StatusInternalServerError, err.Error())
+				return
+			}
+		*/
+
+		/*
+
+
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			jwt := model.Token{}
+
+			jwt.AccessToken, err = token.SignedString([]byte(os.Getenv("TOKEN_SECRET_KEY")))
+			if err != nil {
+				return jwt, err
+			}
+
+			c.Redirect(http.StatusFound, JITSI_URL+"/room/"+room)
+
+			//c.String(http.StatusOK, "OK")
+		*/
+		//c.JSON(http.StatusOK, resp)
+	})
+
+	r.Run(":3001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	/*
+		http.HandleFunc("/", handleStart)
+
+		http.HandleFunc("/callback", handleCallback)
+
+		log.Printf("listening on http://%s/", "0.0.0.0:3001")
+		log.Fatal(http.ListenAndServe(":3001", nil))
+	*/
 }
